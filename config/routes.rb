d640c7ab5f49020a1a0e0004799fc20a3ad8fb16
KazusaNakagawa <kazusanakagawa@gmail.com>
2020-08-29 16:15:19 +0900

    init app

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,3 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-  # get '/articles'
-
-  post 'articles/new'
-
-
-
-  resources :articles
- #  resources :products
-
-	# root 'products#index'
-	root 'welcome#index'
 
 end
