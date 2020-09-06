2429baf598c5f6b05835c37ba244c0b1a362aa47
KazusaNakagawa <kazusanakagawa@gmail.com>
2020-09-06 12:32:57 +0900

    implement static-pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
-  # get 'welcome/index'
-
-  # resources :articles do
-  # 	resources :comments
-  # end	
-  
-  # root 'welcome#index'
   root 'application#hello'
 end
